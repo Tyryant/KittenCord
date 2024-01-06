90beaf35679ec35c90db85eae9055403e676e79a
GunCoo <austincarbone@outlook.com>
2024-01-06 13:48:01 -0500

    messing around with buttons

--- KittenForms/src/App.jsx
+++ KittenForms/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div>
+        <Button variant="contained" disabled="true">
+          {" "}
+          Hello World
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
